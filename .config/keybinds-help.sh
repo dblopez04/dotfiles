e108fd8584c63001d32381690a796d9c040bda13
daniel <daniel@xtal.gay>
2025-05-07 01:04:31 -0500

    1

---
+++ .config/keybinds-help.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo -e "\n\033[1;33m========= Hyprland =========\033[0m"
+
+echo -e "\n\033[1;37mSuper + Q\033[0m        → Launch terminal"
+echo -e "\033[1;37mSuper + E\033[0m        → Launch file manager"
+echo -e "\033[1;37mSuper + R\033[0m        → Launch application menu"
+
+echo -e "\n\033[1;37mSuper + C\033[0m        → Kill active window"
+echo -e "\033[1;37mSuper + M\033[0m        → Exit Hyprland"
+
+echo -e "\n\033[1;37mSuper + Arrows\033[0m   → Move focus"
+echo -e "\033[1;37mSuper + 1-9\033[0m      → Change workspace"
+echo -e "\033[1;37mSuperShift + 1-9\033[0m → Move active window to workspace"
+echo -e "\033[1;37mSuper + MWHEEL\033[0m   → Scroll through active workplaces"
+
+echo -e "\n\033[1;37mSuper + S\033[0m        → Toggle scratchpad"
+echo -e "\033[1;37mSuperShift + S\033[0m   → Move active window to scratchpad"
+
+echo -e "\n\033[1;37mSuper + P\033[0m        → Pseudotiling"
+echo -e "\033[1;37mSuper + J\033[0m        → Toggle vertical/horizontal split"
+
+echo -e "\n\033[1;37mSuper + V\033[0m        → Toggle floating window"
+echo -e "\033[1;37mSuper + LMB\033[0m      → Move floating window"
+echo -e "\033[1;37mSuper + RMB\033[0m      → Resize floating window"
+
+echo -e "\n\033[1;33m========== Kitty ===========\033[0m"
+echo -e "\n\033[1;37mCTRL + Left\033[0m      → Jump to the left"
+echo -e "\033[1;37mCTRL + RMB\033[0m       → Jump to the right"
+
+echo -e "\n\033[1;37mCTRL + Shift + T\033[0m → New tab"
+echo -e "\033[1;37mCTRL + Shift + W\033[0m → Close tab"
+echo -e "\033[1;37mCTRL + Shift + N\033[0m → New window"
+echo -e "\033[1;37mCTRL + Shift + Q\033[0m → Close window"
---
+++ .config/wpg/wp_init.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wpg -rs 'garden-at-arles.png' 'garden-at-arles.png'
